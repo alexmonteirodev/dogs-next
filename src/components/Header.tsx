587ad8f7bf8d0daf3bcc9328bc58d6bb9a2dadec
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-07-16 16:36:05 +0200

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
+
+const Header = () => {
+  const user = true;
+  return (
+    <header className={styles.header}>
+      <nav className={`container ${styles.nav}`}>
+        <Link className={styles.logo} href="/">
+          <Image
+            src={"/assets/dogs.svg"}
+            alt="dogs"
+            width={28}
+            height={22}
+            priority
+          />
+        </Link>
+        {user ? (
+          <Link className={styles.login} href="/conta">
+            dogs
+          </Link>
+        ) : (
+          <Link className={styles.login} href="/login">
+            Login / Criar
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
