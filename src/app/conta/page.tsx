ac7af4c4b121f2fa6c9c1469b62ca4082ae1e2c0
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-07-27 10:38:09 +0200

    context

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
+import { useUser } from "@/context/user-context";
 
 const ContaPage = () => {
-  return <div>ContaPage</div>;
+  const { user } = useUser();
+  console.log(user);
+  return <div>ContaPage_ {user?.nome}</div>;
 };
 
 export default ContaPage;
