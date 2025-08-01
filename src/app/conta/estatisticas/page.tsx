fa1c6a4788a0ddf8e2a643c0e1921d8d1af4cef1
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-08-01 09:23:30 +0200

    lazy loading + estatisticas page

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,11 +1,29 @@
+import statsGet from "@/actions/stats-get";
+// import ContaEstatisticas from "@/components/conta/conta-estatisticas";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
+
+//lazy loading, para carregar o grafico if o user abrir a aba de graficos
+const ContaEstatisticas = dynamic(
+  () => import("@/components/conta/conta-estatisticas"),
+  {
+    loading: () => <p>Carregando...</p>,
+  }
+);
 
 export const metadata: Metadata = {
   title: "Estatísticas",
 };
 
-const EstatisticasPage = () => {
-  return <div>EstatisticasPage</div>;
+const EstatisticasPage = async () => {
+  const { data } = await statsGet();
+  console.log("data", data);
+  if (!data) return;
+  return (
+    <section>
+      <ContaEstatisticas data={data} />
+    </section>
+  );
 };
 
 export default EstatisticasPage;
